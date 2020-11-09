e5f6f83eca51b5e346ab6929c7bea6e8a851166e
LeroyThomas <71132272+LeroyThomas@users.noreply.github.com>
2020-11-09 21:38:44 +0100

    Update cart.php

--- quetes_php_cookies_sessions/cart.php
+++ quetes_php_cookies_sessions/cart.php
@@ -1,12 +1,26 @@
-<?php require 'inc/head.php'; ?>
-<section class="cookies container-fluid">
-    <div class="row">
-        <h2>Votre Pannier</h2>
-        <ul>
-            <?php foreach ($_COOKIE as $id => $cookie) : ?>
-                <p><?php echo $id !== 'PHPSESSID' ?  'il y a ' . ' ' .  $cookie['name'] . ' ' . $id . ' dans votre pannier '  : '' ?></p>
-            <?php endforeach ?>
-        </ul>
- </div>
-</section>
+<?php
+session_start ();
+if (!isset($_SESSION['loginname'])) {
+    header ('Location: login.php');
+}
+require 'inc/data/products.php';
+require 'inc/head.php';
+?>
+
+<?php if(isset($_SESSION['cart'])) : ?>
+    <section class="cookies container-fluid">
+        <div class="row">
+            <?php foreach ($_SESSION['cart'] as $id) { ?>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <figure class="thumbnail text-center">
+                    <img src="assets/img/product-<?= $id; ?>.jpg" alt="<?= $catalog[$id]['name']; ?>" class="img-responsive">
+                    <figcaption class="caption">
+                        <h3><?= $catalog[$id]['name']; ?></h3>
+                    </figcaption>
+                </figure>
+            </div>
+            <?php } ?>
+        </div>
+    </section>
+<?php endif; ?>
 <?php require 'inc/foot.php'; ?>
